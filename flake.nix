a171453d392a58a250356f7b6c2d4a1a42564645
RemcoSchrijver <remco.schrijver@hotmail.com>
2024-09-03 22:35:30 +0200

    Setup nix environment

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Flutter development";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+          };
+        };
+      in
+      {
+        devShell =
+          with pkgs; mkShell rec {
+            buildInputs = [
+              flutter323
+              jdk17
+            ];
+          };
+      });
+}
