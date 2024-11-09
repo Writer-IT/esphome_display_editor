6b4937220ae81c8638ca40ff6d7428a6ba371e4f
RemcoSchrijver <remco.schrijver@hotmail.com>
2024-11-09 12:57:07 +0100

    chore: Cleaned yaml parser pass for readability

--- lib/interpreter/yaml_parser_pass.dart
+++ lib/interpreter/yaml_parser_pass.dart
@@ -7,57 +7,52 @@ import 'package:yaml/yaml.dart';
 /// Pass that parsers the yaml and decides what exactly we're going to do.
 /// Handles variable parsing and intepreting, as well as passing this context to
 /// the render object pass.
-class YamlParserPass {
-  /// First parser pass, splits out the yaml and delegates parsing passes.
-  YamlParserPass();
 
-  /// Parses the code given in [input].
-  List<DisplayObject> parse(String input) {
-    final yaml = loadYaml(input);
-    if (verifyDisplayComponent(yaml as YamlMap)) {
-      late YamlMap display;
-      final displayYaml = yaml['display'];
+/// Parses the code given in [input].
+List<DisplayObject> parse(String input) {
+  final yaml = loadYaml(input);
+  if (verifyDisplayComponent(yaml as YamlMap)) {
+    final codeLines = extractCode(yaml);
 
-      if (displayYaml is YamlMap) {
-        display = displayYaml;
-      } else if (displayYaml is List) {
-        display = displayYaml[0] as YamlMap;
-      }
-      final codeLines = (display['lambda'] as String).split('\n');
+    final (variableToObjectMapping, passedCodeLines) =
+        VariablePass().processDisplayCode(codeLines);
 
-      final variablePass = VariablePass();
+    final parsedDisplayObjects = DisplayParserPass(variableToObjectMapping)
+        .parseDisplayCode(passedCodeLines);
 
-      final (variableToObjectMapping, passedCodeLines) =
-          variablePass.processDisplayCode(codeLines);
-
-      final displayParserPass = DisplayParserPass(variableToObjectMapping);
-
-      final displayObjectPass = DisplayObjectPass();
-
-      final parsedDisplayObjects =
-          displayParserPass.parseDisplayCode(passedCodeLines);
-      return displayObjectPass.transformObjects(
-        parsedDisplayObjects,
-        variableToObjectMapping,
-      );
-    } else {
-      throw const FormatException('This was not valid yaml');
-    }
+    return DisplayObjectPass().transformObjects(
+      parsedDisplayObjects,
+      variableToObjectMapping,
+    );
+  } else {
+    throw const FormatException('This was not valid yaml');
   }
+}
 
-  /// Check if we have a valid display component with a lambda to render.
-  static bool verifyDisplayComponent(YamlMap yaml) {
-    if (yaml.containsKey('display')) {
-      final display = yaml['display'];
-      if (display is YamlMap && display.containsKey('lambda')) {
+/// Check if we have a valid display component with a lambda to render.
+bool verifyDisplayComponent(YamlMap yaml) {
+  if (yaml.containsKey('display')) {
+    final display = yaml['display'];
+    if (display is YamlMap && display.containsKey('lambda')) {
+      return true;
+    } else if (display is List && display.isNotEmpty) {
+      final firstDisplay = display[0];
+      if (firstDisplay is YamlMap && firstDisplay.containsKey('lambda')) {
         return true;
-      } else if (display is List && display.isNotEmpty) {
-        final firstDisplay = display[0];
-        if (firstDisplay is YamlMap && firstDisplay.containsKey('lambda')) {
-          return true;
-        }
       }
     }
-    return false;
   }
+  return false;
+}
+
+/// Extract the code section for the display, and returns them as codelines.
+List<String> extractCode(YamlMap yaml) {
+  late YamlMap display;
+  final displayYaml = yaml['display'];
+  if (displayYaml is YamlMap) {
+    display = displayYaml;
+  } else if (displayYaml is List) {
+    display = displayYaml[0] as YamlMap;
+  }
+  return (display['lambda'] as String).split('\n');
 }
