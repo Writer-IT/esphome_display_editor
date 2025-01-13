67e8fb2e36015e2163bee99fb1480c22fef18242
RemcoSchrijver <remco.schrijver@hotmail.com>
2025-01-13 08:25:01 +0100

    Fixed Formatting of docs for ParsedDisplayObject

--- lib/interpreter/parsed_display_object.dart
+++ lib/interpreter/parsed_display_object.dart
@@ -3,10 +3,10 @@ import 'package:esphome_display_editor/objects/display_object_types.dart';
 /// Mark a line of code to be treated as a render object.
 class ParsedDisplayObject {
   /// We know we will have to render this object, it has a [type] and the
-  /// associated [variables]. 
+  /// associated [variables].
   ///
-  /// Requires the [type] to be defined, the related [variables]. And lastly 
-  /// the special variable [filled], because this has to be gathered from the 
+  /// Requires the [type] to be defined, the related [variables]. And lastly
+  /// the special variable [filled], because this has to be gathered from the
   /// function call.
   ParsedDisplayObject(this.type, this.variables, this.filled);
 
