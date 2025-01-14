f109a0081ae2b12db3aae8111d5ed997e65711d7
RemcoSchrijver <remco.schrijver@hotmail.com>
2025-01-14 08:50:20 +0100

    Added cache for input text, improving QoL

--- lib/esp_home_editor.dart
+++ lib/esp_home_editor.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:esphome_display_editor/esp_home_renderer.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 /// Editor for ESPHome displays.
 class EspHomeEditor extends StatefulWidget {
@@ -11,54 +14,76 @@ class EspHomeEditor extends StatefulWidget {
 }
 
 class _EspHomeEditorState extends State<EspHomeEditor> {
+  static const _displayCodeKey = 'display_code';
   String code = '';
+
   @override
   Widget build(BuildContext context) {
     final textController = TextEditingController(text: code);
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('ESP Home Editor'),
+    return FutureBuilder<SharedPreferencesWithCache>(
+      // ignore: discarded_futures
+      future: SharedPreferencesWithCache.create(
+        cacheOptions: const SharedPreferencesWithCacheOptions(),
       ),
-      body: Center(
-        child: Row(
-          children: [
-            Expanded(
-              child: Column(
+      builder: (context, prefs) {
+        if (prefs.hasData) {
+          var code = prefs.data!.getString(_displayCodeKey) ?? '';
+          textController.text = code;
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+              title: const Text('ESP Home Editor'),
+            ),
+            body: Center(
+              child: Row(
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                    child: TextField(
-                      controller: textController,
-                      keyboardType: TextInputType.multiline,
-                      maxLines: 40,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Your display lambda',
-                      ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                          child: TextField(
+                            controller: textController,
+                            keyboardType: TextInputType.multiline,
+                            maxLines: 40,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: 'Your display lambda',
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                          child: ElevatedButton(
+                            onPressed: () => setState(() {
+                              code = textController.text;
+                              unawaited(
+                                prefs.data!.setString(
+                                  _displayCodeKey,
+                                  code,
+                                ),
+                              );
+                            }),
+                            child: const Text('Render preview'),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                    child: ElevatedButton(
-                      onPressed: () => setState(() {
-                        code = textController.text;
-                      }),
-                      child: const Text('Render preview'),
+                  Expanded(
+                    child: EspHomeRenderer(
+                      code: code,
+                      variables: const [],
                     ),
                   ),
                 ],
               ),
             ),
-            Expanded(
-              child: EspHomeRenderer(
-                code: code,
-                variables: const [],
-              ),
-            ),
-          ],
-        ),
-      ),
+          );
+        } else {
+          return const Text('Please wait');
+        }
+      },
     );
   }
 }
